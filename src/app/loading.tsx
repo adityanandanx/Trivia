b12108f84d145b218dcf55a945a715cbde64f85d
iMADi-ARCH <nandanaditya985@gmail.com>
2023-04-19 15:34:44 +0530

    fun loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,12 +1,27 @@
+"use client";
 import Spinner from "@/components/ui/Spinner";
+import { shuffle } from "@/lib/utils";
 import { FC } from "react";
 
 interface LoadingProps {}
 
 const Loading: FC<LoadingProps> = ({}) => {
+    const loadingPhrases = [
+        "Painting a masterpiece",
+        "Brewing a fresh pot of coffee",
+        "Sharpening a set of knives",
+        "Assembling a jigsaw puzzle",
+        "Planting a vegetable garden",
+        "Tuning a guitar",
+        "Organizing a closet",
+        "Building a sandcastle",
+        "Folding a stack of laundry",
+        "Mixing a cocktail",
+    ];
+
     return (
-        <div className="flex items-center gap-3">
-            <Spinner />
+        <div className="flex flex-col items-center gap-3">
+            {shuffle(loadingPhrases)[0]} <Spinner />
         </div>
     );
 };
